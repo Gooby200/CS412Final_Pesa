be5c5c608501ee050e6f3ebe0e01279280adca5d
Gooby200 <rgpesa@gmail.com>
2021-06-14 20:42:48 -0500

    Image Upload Example

---
+++ CS412Final_Pesa/Examples/ImageUploadExample.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Public.Master" AutoEventWireup="true" CodeBehind="ImageUploadExample.aspx.cs" Inherits="CS412Final_Pesa.Examples.ImageUploadExample" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div>
+        <label>Upload Image</label>
+        <asp:FileUpload ID="FileUpload1" runat="server" accept="image/*" />
+        <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+    </div>
+    <div class="row">
+        <asp:Repeater ID="Repeater1" runat="server" OnItemDataBound="Repeater1_ItemDataBound">
+            <ItemTemplate>
+                <div class="col-4">
+                    <asp:Image ID="Image1" Width="100%" runat="server" ImageUrl="<%# Container.DataItem %>" />
+                </div>
+            </ItemTemplate>
+        </asp:Repeater>
+    </div>
+</asp:Content>
---
+++ CS412Final_Pesa/Examples/ImageUploadExample.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS412Final_Pesa.Examples {
+    public partial class ImageUploadExample : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            if (Page.IsPostBack == false) {
+                BindImageRepeater();
+            }
+        }
+
+        protected void Repeater1_ItemDataBound(object sender, RepeaterItemEventArgs e) {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e) {
+            string strFolder = Server.MapPath("./user_images/");
+            string strFileName = FileUpload1.PostedFile.FileName;
+
+            //method 1
+            FileUpload1.SaveAs(strFolder + strFileName);
+
+            BindImageRepeater();
+
+            //method 2
+            //string strFileName = FileUpload1.PostedFile.FileName;
+            //strFileName = Path.GetFileName(strFileName);
+        }
+
+        private void BindImageRepeater() {
+            List<string> imagePaths = new List<string>();
+            string strFolder = Server.MapPath("./user_images/");
+
+            string[] filesindirectory = Directory.GetFiles(strFolder);
+            foreach (string item in filesindirectory) {
+                imagePaths.Add($"user_images/{Path.GetFileName(item)}");
+            }
+
+            Repeater1.DataSource = imagePaths;
+            Repeater1.DataBind();
+        }
+    }
+}
